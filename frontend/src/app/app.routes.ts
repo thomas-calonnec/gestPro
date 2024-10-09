4ceaf13fefd146640ac800485a9be4f690fbe4c3
thomas-calonnec <thomas.calonnec@gmail.com>
2024-10-09 16:26:39 +0200

    update frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { BoardComponent } from './board/board.component';
-import { ListCardComponent } from './list-card/list-card.component';
-import { AppComponent } from './app.component';
 
-export const routes: Routes = [
-    {path: 'board', component:BoardComponent},
-    {path: 'listcard', component: ListCardComponent},
-  
-];
+export const routes: Routes = [];
