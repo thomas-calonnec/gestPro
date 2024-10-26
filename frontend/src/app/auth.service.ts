1b40fb7f3ca5ca407807533d18ed136b969fbe22
thomas-calonnec <thomas.calonnec@gmail.com>
2024-10-26 13:24:37 +0200

    update controller 2

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,10 +1,12 @@
-import {effect, inject, Injectable, signal} from '@angular/core';
+import {effect, inject, Injectable, NgZone, signal} from '@angular/core';
 import {catchError, Observable, tap, throwError} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
-import {jwtDecode} from 'jwt-decode';
 import {environment} from '@environments/environment.development';
+import {Token} from '@models/token';
+import {jwtDecode} from 'jwt-decode';
 
+declare const google: any;
 
 @Injectable({
   providedIn: 'root',
@@ -17,7 +19,7 @@ export class AuthService {
   private router : Router = inject(Router);
   private http: HttpClient = inject(HttpClient);
 
-  constructor() {
+  constructor(private ngZone: NgZone) {
     // Effet qui écoute l'accessToken pour le stocker dans localStorage quand il change
     effect(() => {
       const accessToken = this.accessTokenSignal();
@@ -68,18 +70,18 @@ export class AuthService {
   }
 
   // Method to check if token is expired based on `exp` field
-  isTokenExpired(token: string): boolean {
-    const decoded: any = jwtDecode(token);
-    if (!decoded.exp) return true; // Treat as expired if no expiry field
+  isTokenExpired(token: Token): boolean {
+    //const decoded: any = jwtDecode(token);
+    if (!token.exp) return true; // Treat as expired if no expiry field
 
-    const expiryDate = new Date(decoded.exp * 1000); // Convert `exp` to milliseconds
+    const expiryDate = new Date(token.exp * 1000); // Convert `exp` to milliseconds
     return new Date() > expiryDate;
   }
 
-  hasValidAccessToken(): boolean {
-    const token = this.getAccessToken();
-    return !!token && !this.isTokenExpired(token);
-  }
+  // hasValidAccessToken(): boolean {
+  //   const token = this.getAccessToken();
+  //   return !!token && !this.isTokenExpired(token);
+  // }
 
   refreshAccessToken(): Observable<any> {
 
@@ -103,18 +105,67 @@ export class AuthService {
     return this.http.post<any>(`${this.apiServerUrl}/login`, { username, password }, {withCredentials: true})
       .pipe(
         tap((response) => {
-            this.setAccessToken(response.accessToken)
+          this.setAccessToken(response.accessToken)
         })
-
-
       );
-
-
   }
 
   logout() {
     this.removeTokens();
     localStorage.removeItem('workspaceId');
     this.router.navigate(['/login']);
+  }
+
+  getOAuthGoogle(idToken: string):Observable<any> {
+
+    return this.http.post<any>(`${this.apiServerUrl}/oauth2`, { token: idToken }, // Payload
+      { headers: { 'Content-Type': 'application/json' }
+      })
+  }
+
+
+  initializeGoogleAuth(): void {
+    // Vérifiez si la bibliothèque Google est chargée
+    if (typeof google === 'undefined') {
+      console.error('Google Identity Services n’est pas chargé.');
+      return;
+    }
+
+    google.accounts.id.initialize({
+      client_id: '392803604648-s1fsi45jpjictprj577ev1akjtcivopr.apps.googleusercontent.com',
+      callback: (response: any) => this.handleCredentialResponse(response),
+      use_fedcm_for_prompt: true, // Activer FedCM
+    });
+
+    // Rendre le bouton Google Sign-In
+
+
+    // Optionnel : affichage automatique du prompt
+    google.accounts.id.prompt();
+  }
+
+  // Fonction callback pour gérer la réponse après authentification
+  handleCredentialResponse(response: any): void {
+    this.ngZone.run(() => {
+      console.log('Token JWT reçu via FedCM :', response.credential);
+
+      // Envoyer le token au backend si nécessaire
+      this.verifyTokenWithBackend(response.credential);
+    });
+  }
+
+  verifyTokenWithBackend(idToken: string) {
+
+
+    this.getOAuthGoogle(idToken).subscribe({
+      next: googleResponse => {
+
+        this.setAccessToken(googleResponse.expiration);
+        this.router.navigate([`users/${googleResponse.googleId}/workspaces`]);
+
+        // window.location.href = `users/${googleResponse.googleId}/workspaces`;
+      }
+    })
+
   }
 }
