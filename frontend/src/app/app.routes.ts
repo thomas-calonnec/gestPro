cace6df0e0983385629e1885bff40c095b74227c
thomas-calonnec <thomas.calonnec@gmail.com>
2024-10-31 15:55:53 +0100

    activate Guard angular login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,16 @@ import {ListCardComponent} from './list-card/list-card.component';
 import {WorkspaceComponent} from './workspace/workspace.component';
 import { LoginComponent } from './login/login.component';
 import {UserComponent} from './user/user.component';
+import {MainComponent} from './main/main.component';
+import {AuthGuard} from './auth-guard';
 
 export const routes: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {path: 'workspaces/:id/boards', component: WorkspaceComponent, data: { id: 'string'}},
-  {path: 'board', component: BoardComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'users/:id/workspaces', component: UserComponent, data: {id: 'string'}},
-  {path: 'listCard', component: ListCardComponent}
+  {path: '',component: MainComponent, canActivate: [AuthGuard], children : [
+      {path: 'workspaces/:id/boards', component: WorkspaceComponent, data: { id: 'string'}},
+      {path: 'board', component: BoardComponent},
+      {path: 'users/:id/workspaces', component: UserComponent, data: {id: 'string'}},
+      {path: 'listCard', component: ListCardComponent}
+    ]},
+  {path: 'login', component: LoginComponent}
+
 ];
