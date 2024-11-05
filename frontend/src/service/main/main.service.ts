98dcfb31155a98f79adb24d011290f3b2b765c27
thomas-calonnec <thomas.calonnec@gmail.com>
2024-11-05 21:57:09 +0100

    add sidebar and main service

---
+++ frontend/src/service/main/main.service.ts
@@ -0,0 +1,27 @@
+import {Injectable, Signal, signal, WritableSignal} from '@angular/core';
+import {Workspace} from '../../dao/workspace';
+import {Board} from '../../dao/board';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainService {
+
+  public workspace : Workspace | undefined;
+  public boards : Signal<Board[]> = signal<Board[]>([]);
+ //public boards : Board[] = []
+  public setWorkspace(workspace : Workspace) {
+    this.workspace = workspace;
+  }
+  public getWorkspace() : Workspace | undefined{
+    return this.workspace;
+  }
+
+  setBoards(boards : Board[]) {
+   // this.boards.bind(boards);
+    this.boards.bind(boards);
+  }
+  getListBoards(): Board[] {
+    return this.boards();
+  }
+}
