fd0d2d6b0e4b603cde90f459d443139351026c5f
thomas-calonnec <thomas.calonnec@gmail.com>
2024-11-06 00:14:32 +0100

    ReUpload project

--- backend/src/main/java/com/thomas/gestPro/Security/WebConfig.java
+++ backend/src/main/java/com/thomas/gestPro/Security/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permettre CORS sur tous les chemins
-                .allowedOrigins("http://192.168.1.138:4200") // Autoriser Angular
+                .allowedOrigins("http://localhost:4200") // Autoriser Angular
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Méthodes HTTP autorisées
                 .allowedHeaders("Authorization", "Content-Type") // Autoriser les en-têtes d'authentification
                 //.exposedHeaders("Authorization") // Facultatif si vous devez exposer cet en-tête dans la réponse
-                .allowCredentials(true); // Si vous avez des cookies de session (sinon, pas nécessaire)
+                .allowCredentials(true);
     }
 }
