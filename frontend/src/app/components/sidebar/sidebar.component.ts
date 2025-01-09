fcb9d7097e599ea6508cbd988f706158a4e63edf
thomas-calonnec <thomas.calonnec@gmail.com>
2025-01-09 18:34:06 +0100

    Fix: remove deprecated version of packages

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -3,6 +3,7 @@ import {RouterLink} from '@angular/router';
 import {MainService} from '../../../service/main/main.service';
 import {WorkspaceService} from '../../../service/workspaces/workspace.service';
 import {Board} from '../../../dao/board';
+import {AuthService} from '../../auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -16,13 +17,28 @@ import {Board} from '../../../dao/board';
 export class SidebarComponent implements OnInit{
   workspaceService: WorkspaceService = inject(WorkspaceService);
   mainService : MainService = inject(MainService);
-  workspaceId: string | null= ""
+  workspaceId: string | null = "";
+  workspaceName: string = "";
+  authService: AuthService = inject(AuthService)
 
   ngOnInit() {
+    this.workspaceId = localStorage.getItem('workspaceId');
     this.getBoards()
+
+  this.getWorkspace()
+  }
+  getWorkspace() {
+    this.workspaceService.getWorkspaceById(this.workspaceId).subscribe({
+      next: (workspace) => {
+        this.workspaceName = workspace.name;
+      }
+    })
+  }
+  logout() {
+    this.authService.logout();
   }
   getBoards() {
-    this.workspaceId = localStorage.getItem('workspaceId');
+
 
     if(this.workspaceId !== null) {
       this.workspaceService.getBoards(this.workspaceId).subscribe({
