11817fa1c4e4245686f0cac2b7664b87503601a6
thomas-calonnec <thomas.calonnec@gmail.com>
2025-01-10 16:42:29 +0100

    Fix: update Jenkinsfile

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi} from '@angular/common/http';
-import {AuthInterceptor} from '@/services/auth-interceptor/auth-interceptor.service';
+import {AuthInterceptor} from '@services/auth-interceptor/auth-interceptor.service';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
