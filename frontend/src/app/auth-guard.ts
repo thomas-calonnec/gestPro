d3c832714c93624874358757b81dec6615204303
thomas-calonnec <thomas.calonnec@gmail.com>
2025-01-28 23:55:56 +0100

    Fix: change application.properties

--- frontend/src/app/auth-guard.ts
+++ frontend/src/app/auth-guard.ts
@@ -1,30 +1,17 @@
 import {inject} from '@angular/core';
 import {AuthService} from '@services/auth/auth.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {UserService} from '@services/users/user.service';
-import {catchError, map, Observable} from 'rxjs';
+import {Router} from '@angular/router';
 
-export const AuthGuard   = (): Observable<boolean> => {
+export const AuthGuard  = () => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
 
-    const authService = inject(AuthService);
-    const router : Router = inject(Router);
-    return authService.isAuthenticated().pipe(
-      map((isAuthenticated) => {
-        if (isAuthenticated) {
-          return true; // L'utilisateur est authentifié
-        } else {
-          router.navigate(['/login']); // Rediriger l'utilisateur vers la page de connexion
-          return false;
-        }
-      }),
-      catchError(() => {
-        // En cas d'erreur (par exemple, problème de cookie), rediriger vers la page de connexion
-        router.navigate(['/login']);
-        return [false];
-      })
-    );
-
-
+  if(!auth.isAuthenticated()) {
+    auth.removeTokens();
+    router.navigateByUrl('/login').then(r => r);
+    return false;
+  }
+  return true;
 
 
 }
